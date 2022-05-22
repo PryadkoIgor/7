83a147de0f7d8ba733bd29fe2656bba05bdcae10
ROGI <impik@list.ru>
2022-05-22 21:25:55 +0300

    дз 7

---
+++ Задача 47/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте двумерный массив размером M x N, заполненный случайными вещественными числами.
+// M = 3, N = 4
+// 0,5 7 -2 0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7 1,9
+
+Console.Clear();
+
+double[,]array = new double[3, 4];
+ 
+for (int i = 0; i < 3; i++)
+{
+     for (int j = 0; j <4; j++)
+    {
+         array[i, j] = new Random().NextDouble() * new Random().Next(-20, 20);
+         Console.Write("{0}\t", array[i, j]);
+    }   
+    Console.WriteLine();
+}
+
